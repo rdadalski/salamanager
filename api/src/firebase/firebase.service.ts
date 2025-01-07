25c17fac422af49399ee696037fc1338521d1f4a
Maciej Dądalski <dadalski.maciej@gmail.com>
2025-01-07 12:42:44 +0100

    SM-23 Generic firestore CRUD service

--- api/src/firebase/firebase.service.ts
+++ api/src/firebase/firebase.service.ts
@@ -1,42 +1,15 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
-import { app } from 'firebase-admin';
 
 @Injectable()
 export class FirebaseService {
-  private db: admin.firestore.Firestore;
-  #collection: FirebaseFirestore.CollectionReference;
-
-  constructor(
-    @Inject('FIREBASE_ADMIN') private readonly firebaseAdmin: admin.app.App,
-  ) {
-    this.db = this.firebaseAdmin.firestore();
-  }
-
-  async addItem(data: any) {
-    console.log(data);
-
-    try {
-      const docRef = await this.db.collection('testCollection').add(data);
-      return { id: docRef.id, ...data }; // Return the document ID and data
-    } catch (error) {
-      console.error('Error adding document:', error);
-      throw error;
-    }
-  }
-
+  // this is for firebase verification used in FirebaseAuthGuard
+  // it will be moved later when we take care of authentication
   async verifyToken(idToken: string): Promise<admin.auth.DecodedIdToken> {
     try {
       return await admin.auth().verifyIdToken(idToken);
     } catch (error) {
       throw new Error('Invalid token');
     }
-  }
-
-  async createUser(
-    email: string,
-    password: string,
-  ): Promise<admin.auth.UserRecord> {
-    return admin.auth().createUser({ email, password });
   }
 }
---
+++ api/src/test-generic/test-generic.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UpdateTestGenericDto } from './dto/update-test-generic.dto';
+import { GenericFirestoreService } from '@app/firebase/generic-firestore.service';
+
+interface ITestCrudCollection {
+  date: string;
+  number: number;
+  text: string;
+}
+
+@Controller('test-generic')
+export class TestGenericController {
+  constructor(
+    private readonly genericFirestore: GenericFirestoreService<ITestCrudCollection>,
+  ) {}
+
+  @Post()
+  create(@Body() createTestGenericDto: ITestCrudCollection) {
+    return this.genericFirestore.create(createTestGenericDto);
+  }
+
+  @Get()
+  findAll() {
+    console.log('findAll');
+    return this.genericFirestore.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.genericFirestore.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateTestGenericDto: UpdateTestGenericDto,
+  ) {
+    return this.genericFirestore.update(id, updateTestGenericDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.genericFirestore.delete(id);
+  }
+}
