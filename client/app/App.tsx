bfb562560dc52bc00628f965790d23f76da8aef7
Maciej Dądalski <dadalski.maciej@gmail.com>
2025-02-12 08:40:59 +0100

    commmit changes before build

--- client/app/App.tsx
+++ client/app/App.tsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { RootNavigator } from "./navigation";
+import { firebase } from "@react-native-firebase/app";
 
 export default function App() {
   const [isUserSignedIn, setSignedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    const { token } = firebase.appCheck().getToken(true);
+    console.log(token);
+  }, []);
 
   return (
     <>
