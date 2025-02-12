bf23303066cb770029b221211d08b4a8e76c069d
Maciej Dądalski <dadalski.maciej@gmail.com>
2025-02-12 18:13:17 +0100

    cleanup

--- client/app/Screens/NotificationTestScreen.tsx
+++ client/app/Screens/NotificationTestScreen.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { NotificationTest } from "../components/NotificationsTest";
+import { NotificationTest } from "../components";
 
 export const NotificationTestScreen: FC = () => {
   return <NotificationTest />;
